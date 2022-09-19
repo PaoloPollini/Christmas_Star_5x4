20eb0d06e779f3642a9486f7faa7730760ad4985
PaoloPollini <98055908+PaoloPollini@users.noreply.github.com>
2022-09-19 17:55:59 +0300

    Update Christmas_Star_5x5.ino

--- Christmas_Star_5x5.ino
+++ Christmas_Star_5x5.ino
@@ -1,7 +1,7 @@
 /*
 (c)saigon 2022  
 Written: Sep 12 2022.
-Last Updated: Sep 18 2022
+Last Updated: Sep 19 2022
 
 Звезда для новогодней елки на WS2812
 Пять лучей по 5 светодиодов в каждом
@@ -19,9 +19,9 @@ CRGB leds[LED_NUM];
 unsigned long paletteTimer = 0; // таймер показа эффектов
 int showPaletteInterval = 10;   // продолжительность показа выбранного эффекта (сек)
 byte currentPalette=0;          // текущая палитра
-byte counter;
+byte counter= MAX_BRIGHTNESS-1;
 boolean dir=true; 
-
+/*
 // матрица 11х4
 byte glossMatrix[][4] = {
   {7,12,12,12},
@@ -36,7 +36,22 @@ byte glossMatrix[][4] = {
   {21,23,23,23},
   {22,22,22,22},
 };
+*/
 
+// матрица 11х4
+byte glossMatrix[][4] = {
+  {2,22,22,22},
+  {1,23,23,23},
+  {3,0,24,21},
+  {4,20,20,20},
+  {5,19,19,19},
+  {6,18,18,18},
+  {7,8,16,17},
+  {9,15,15,15},
+  {10,14,14,14},
+  {11,13,13,13},  
+  {12,12,12,12},
+};
 
 void setup() {
   Serial.begin(9600);
@@ -48,17 +63,17 @@ paletteTimer = millis();
 }
 
 void loop() {
-/*
+
 if (millis() >= paletteTimer + showPaletteInterval * 1000) {
  currentPalette++;
- if (currentPalette >2) currentPalette = 0; 
+ if (currentPalette >4) currentPalette = 0; 
  paletteTimer = millis(); 
 }
 
 switch (currentPalette) {
-*/
 
-switch (3) {  
+
+//switch (4) {  
   case 0:
 
 // ------------------------ Искры -----------------
@@ -100,23 +115,49 @@ switch (3) {
     leds[i].setHue(255);
     leds[LED_NUM-i-1].setHue(255);
   FastLED.show();
-  delay(20);        // время свечения точек
+  delay(30);        // время свечения точек
   }
 
     break;
   case 3:
   
  // ------------------------ Блеск ----------------- 
+
+  for (int i = 0;  i < 11; i++) {
+
   FastLED.clear();
   FastLED.setBrightness(MAX_BRIGHTNESS);
   FastLED.show();
+    
+    for (int j = 0;  j < 4; j++) {
+//     Serial.println(glossMatrix[i][j]);
+//    leds[glossMatrix[i][j]].setHue(255);
+    leds[glossMatrix[10 - i][j]].setHue(255);
 
-  for (int i = 0;  i < 11; i++) {
-     Serial.println(glossMatrix[i][4]);
-    //leds[i].setHue(255);
   }
-  
+  FastLED.show();
+  delay(30);
+ } 
+ 
     break;
+  case 4:  
+// ------------------------ Сердцебиение -----------------
+
+
+  for (int i = 0; i < LED_NUM; i++) {
+    leds[i].setHue(255);
+  }
+
+  FastLED.setBrightness(counter);
+  FastLED.show();
+    if (dir) counter++; // увеличиваем яркость
+    else counter--;     // уменьшаем
+    if (counter >= MAX_BRIGHTNESS || counter <= MAX_BRIGHTNESS/3) dir = !dir;   // разворачиваем
+  if (counter >= MAX_BRIGHTNESS/3 & counter <= MAX_BRIGHTNESS - MAX_BRIGHTNESS/3*2 ) delay(50);    // скорость дыхания
+  else   delay(15);
+    
+    break;
+
 }
 
 }
