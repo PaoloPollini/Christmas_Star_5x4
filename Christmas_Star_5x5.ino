ed44e51713c525bf5bd5f4aef50ebcce00685311
PaoloPollini <98055908+PaoloPollini@users.noreply.github.com>
2022-09-22 16:22:25 +0300

    Pre Final
    Предфинальная версия

--- Christmas_Star_5x5.ino
+++ Christmas_Star_5x5.ino
@@ -1,27 +1,21 @@
 /*
 (c)saigon 2022  
 Written: Sep 12 2022.
-Last Updated: Sep 21 2022
+Last Updated: Sep 22 2022
 
 Звезда для новогодней елки на WS2812
 Пять лучей по 5 светодиодов в каждом
-
 */
 
 #define LED_PIN 2               // пин ленты
 #define LED_NUM 25              // количество светодиодов
-#define COLON_SIZE 4            // высота матрицы
-#define COLON_AMOUNT 5          // ширина матрицы
-#define MIN_BRIGHTNESS 0        // минимальная яркость ленты. Если больше нуля, звезда не будет гаснуть полностью
-#define MAX_BRIGHTNESS 150       // максимальная яркость ленты
+#define MAX_BRIGHTNESS 150      // максимальная яркость ленты
 
 #include "FastLED.h"
 CRGB leds[LED_NUM];
-unsigned long paletteTimer; // таймер показа эффектов
+unsigned long paletteTimer;     // таймер показа эффектов
 int showPaletteInterval = 10;   // продолжительность показа выбранного эффекта (сек)
 byte currentPalette=0;          // текущая палитра
-byte counter= MAX_BRIGHTNESS-1;
-boolean dir=true; 
 
 // матрица 11х4 сверху вниз
 byte glossMatrix[][4] = {
@@ -38,7 +32,7 @@ byte glossMatrix[][4] = {
   {12,12,12,12},
 };
 
-// матрица 11х4 сверху слева вверх направо
+// матрица 11х4 сверху слева вниз направо
 byte glossMatrix2[][4] = {
   {7,12,12,12},
   {8,11,11,11},
@@ -54,7 +48,6 @@ byte glossMatrix2[][4] = {
 };
 
 void setup() {
-  Serial.begin(9600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUM);
   FastLED.setBrightness(MAX_BRIGHTNESS);
   randomSeed(analogRead(0));
@@ -62,8 +55,8 @@ void setup() {
 }
 
 void loop() {
-  
-if (millis() >= paletteTimer + showPaletteInterval * 1000) {
+
+if (millis() >= paletteTimer + showPaletteInterval * 500) {
  currentPalette++;
  if (currentPalette >8) currentPalette = 0; 
  paletteTimer = millis(); 
@@ -71,7 +64,7 @@ if (millis() >= paletteTimer + showPaletteInterval * 1000) {
 
 switch (currentPalette) {  
 
-//switch (4) {  
+//switch (1) {  
   case 0:
   sparks();
     break;
@@ -79,59 +72,100 @@ switch (currentPalette) {
   breathe();
     break;
   case 2:
-  runningDots();
+   gloss();
     break;
   case 3:
-  waterfall();
-    break;
-  case 4:
-  gloss();
-    break;
-  case 5:  
   heartBeat();
     break;
+  case 4:
+  sparkle();
+    break;
+  case 5:  
+  runningDots();
+    break;
   case 6:  
-  snake();
+  waterfall();  
     break;
   case 7:  
-  sparkle();
+  snake();
     break;
   case 8:  
   flashingRays();
     break;
 }
 }
-// ----------- END LOOP --------------------
 
+// ============================= ЭФФЕКТЫ ==============================
 
-// ======================= ЭФФЕКТЫ =========================
-
-void sparks(){      // ------------------------ Искры -----------------
+void sparks(){        // ------------------------ Искры -----------------
  FastLED.clear();
  FastLED.show();
  delay(100);        // Время паузы
- 
  leds[random(LED_NUM)].setHue(255); // две искры одновременно рандомно
  leds[random(LED_NUM)].setHue(255);
   FastLED.show();
   delay(25);        // Время вспышки
 }
 
-void breathe(){      // ------------------------ Дыхание -----------------
-int min_brightness = 60; // минимальная яркость (ниже 10 - некрасиво мерцает)
+void breathe(){       // ------------------------ Дыхание -----------------
+int min_brightness = 80; // минимальная яркость (ниже 10 - некрасиво мерцает)
 int q;
   for (int j = min_brightness; j < 512 - min_brightness; j++) {
     if (j > 255) q=511-j; else q=j;
   for (int i = 0; i < LED_NUM; i++) {
     leds[i].setHSV(255, 255, q);
   }
- // leds[random(LED_NUM)].setHSV(255, 255, 255);   // блёстки
+ // leds[random(LED_NUM)].setHSV(255, 255, 255);   // искры
  FastLED.show();
-  delay(7);       // частота дыхания
+  delay(7);       // скорость дыхания
 }
 }
 
-void runningDots(){    // ------------------------ Бегущие точки -----------------
+void gloss(){         // ------------------------ Блеск ----------------- 
+  FastLED.clear();
+  FastLED.show();
+  for (int i = 0;  i < 11; i++) {
+   for (int g = 0; g < LED_NUM; g++) {
+    leds[g].setHSV(255, 255, 155);
+  } 
+    for (int j = 0;  j < 4; j++) {
+    leds[glossMatrix2[i][j]].setHSV(255, 255, 255);
+  }
+  FastLED.show();
+  delay(30);
+ }
+  leds[glossMatrix2[10][0]].setHSV(255, 255, 155);
+  FastLED.show();
+  delay(1000+random(20)*100); // рандомно блеск от 1 до 3 сек
+}
+
+void heartBeat(){     // ------------------------ Сердцебиение -----------------
+int min_brightness = 60; // минимальная яркость (ниже 10 - некрасиво мерцает)
+int q;
+  for (int j = min_brightness; j < 512 - min_brightness; j+=2) {
+    if (j > 255) q=511-j; else q=j;
+  for (int i = 0; i < LED_NUM; i++) {
+    leds[i].setHSV(255, 255, q);
+  }
+ FastLED.show();
+ if (j > (255-min_brightness)/3 & j < (255 + (255-min_brightness)/3))
+  delay(2);      // скорость яркой вспышки 
+  else
+   delay(14);    // скорость затухания
+}
+}
+
+void sparkle(){       // ------------------------ Свечение с искрами -----------------
+int background_brightness = 100; // яркость фона
+  for (int i = 0; i < LED_NUM; i++) {
+    leds[i].setHSV(255, 255, background_brightness);
+  }
+ leds[random(LED_NUM)].setHSV(255, 255, 255);   // искры
+ FastLED.show();
+  delay(10);
+}
+
+void runningDots(){   // ------------------------ Бегущие точки -----------------
  for (int i = 0; i < LED_NUM; i++) {
   FastLED.clear();
   FastLED.show();
@@ -143,15 +177,12 @@ void runningDots(){    // ------------------------ Бегущие точки ---
   }
 }
 
-
-void waterfall(){      // ------------------------ Водопад ----------------- 
+void waterfall(){     // ------------------------ Водопад ----------------- 
   for (int i = 0;  i < 11; i++) {
   FastLED.clear();
   FastLED.show();
     
     for (int j = 0;  j < 4; j++) {
-//     Serial.println(glossMatrix[i][j]);
-//    leds[glossMatrix[i][j]].setHue(255);
     leds[glossMatrix[10 - i][j]].setHue(255);
   }
   FastLED.show();
@@ -159,56 +190,17 @@ void waterfall(){      // ------------------------ Водопад --------------
  } 
 }
 
-void gloss(){      // ------------------------ Блеск ----------------- 
-  FastLED.clear();
-  FastLED.show();
-  for (int i = 0;  i < 11; i++) {
-   for (int g = 0; g < LED_NUM; g++) {
-    leds[g].setHSV(255, 255, 155);
-  }   
-    for (int j = 0;  j < 4; j++) {
-    leds[glossMatrix2[i][j]].setHSV(255, 255, 255);
-  }
-  FastLED.show();
-  delay(30);
- }
-  delay(1000+random(20)*100);
-}
-
-void heartBeat(){      // ------------------------ Сердцебиение -----------------
-  for (int i = 0; i < LED_NUM; i++) {
-    leds[i].setHue(255);
-  }
-  FastLED.setBrightness(counter);
-  FastLED.show();
-    if (dir) counter++; // увеличиваем яркость
-    else counter--;     // уменьшаем
-    if (counter >= MAX_BRIGHTNESS || counter <= MAX_BRIGHTNESS/3) dir = !dir;   // разворачиваем
-  if (counter >= MAX_BRIGHTNESS/3 & counter <= MAX_BRIGHTNESS - MAX_BRIGHTNESS/3*2 ) delay(50);    // скорость дыхания
-  else   delay(15);
-}
-
-void snake(){      // ------------------------ Змейка -----------------
+void snake(){         // ------------------------ Змейка -----------------
     for (int j = 0;  j < 25; j++) {
   for (int i = 0; i < LED_NUM; i++) {
     leds[i].setHSV(255, 255, (i-j+50) * 10);
   }
   FastLED.show();
-  delay(30);
+  delay(50);  // скорость змейки
  } 
 }
 
-void sparkle(){      // ------------------------ Свечение с искрами -----------------
-int background_brightness = 100; // яркость фона
-  for (int i = 0; i < LED_NUM; i++) {
-    leds[i].setHSV(255, 255, background_brightness);
-  }
- leds[random(LED_NUM)].setHSV(255, 255, 255);   // блёстки
- FastLED.show();
-  delay(10);
-}
-
-void flashingRays(){    // ------------------------ Мигающие лучи -----------------
+void flashingRays(){  // ------------------------ Мигающие лучи -----------------
 int q;
 int vals[] = {0, 10, 20, 5, 15}; // порядок переключения лучей
 
